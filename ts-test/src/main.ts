8b4ecdad745c0c4692262bf1dc25510e2719c784
coolmarvel <marvel97@naver.com>
2023-12-03 10:51:46 +0900

    type 종류 / string number boolean null undefined array object function study

--- ts-test/src/main.ts
+++ ts-test/src/main.ts
@@ -1,5 +1,66 @@
-let hello: string = "Hello world!";
-console.log(hello);
+// 타입 종류
 
-hello = "Hello Typescript";
-console.log(hello);
+// string
+let str: string;
+let red: string = "red";
+let green: string = "green";
+let myColor: string = `my color is ${red}`;
+let yourColor: string = `your color is ${green}`;
+
+// number
+let num: number;
+let integer: number = 6;
+let float: number = 3.14;
+let infinity: number = Infinity;
+let nan: number = NaN;
+
+// boolean
+let isBolean: boolean;
+let isDone: boolean = false;
+
+// null / undefined
+let nul: null;
+let und: undefined;
+
+// array
+const fruits: string[] = ["apple", "banana", "cherry"];
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 7];
+const union: (string | number)[] = ["apple", 2, 3, "banana", 4, 5, "cherry"];
+
+// object (typeof DATA === 'object')
+const obj: object = {};
+const arr: object = [];
+const func: object = function () {};
+
+interface User {
+  name: string;
+  age: number;
+  isValid: boolean;
+}
+const userA: User = { name: "coolmarvel", age: 27, isValid: true };
+const userB: { name: string; age: number; isValid: boolean } = { name: "mykim", age: 4, isValid: false };
+
+// function
+const add: (x: number, y: number) => number = function (x, y) {
+  return x + y;
+};
+const a: number = add(1, 2);
+
+const hello: () => void = () => {
+  console.log("hello world!");
+};
+const h: void = hello();
+
+// any
+
+// unknown
+
+// tuple
+
+// void
+
+// never
+
+// union
+
+// intersection
