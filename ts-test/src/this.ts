9be19840f440fd99a18d8816fe8f435f249bb6d0
coolmarvel <marvel97@naver.com>
2023-12-05 10:56:25 +0900

    명시적 this commit

---
+++ ts-test/src/this.ts
@@ -0,0 +1,16 @@
+// 함수 - 명시적 this
+
+// interface Cat {
+//   name: string;
+//   age: number;
+// }
+
+// const cat: Cat = {
+//   name: "aeong",
+//   age: 4,
+// };
+
+// function hello(this: Cat, params: string): void {
+//   console.log(`Hello ${this.name}, ${params}`);
+// }
+// hello.call(cat, "you are pretty awesome");
