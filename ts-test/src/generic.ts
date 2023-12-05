7694a0f5f67479477b510a1b00c8818260d8ca87
coolmarvel <marvel97@naver.com>
2023-12-05 16:03:04 +0900

    generic type class concept commit

--- ts-test/src/generic.ts
+++ ts-test/src/generic.ts
@@ -1,10 +1,10 @@
 // Generic
 // Function
 
-interface Obj {
-  x: number;
-}
-type Arr = [number, number];
+// interface Obj {
+//   x: number;
+// }
+// type Arr = [number, number];
 
 // function toArray(a: string, b: string): string[];
 // function toArray(a: number, b: number): number[];
@@ -12,13 +12,43 @@ type Arr = [number, number];
 // function toArray(a: Obj, b: Obj): Obj[];
 // function toArray(a: Arr, b: Arr): Arr[];
 
-function toArray<T>(a: T, b: T) {
-  return [a, b];
+// function toArray<T>(a: T, b: T) {
+//   return [a, b];
+// }
+
+// console.log(toArray<number>(1, 2));
+// console.log(toArray<string>("lee", "seonghyun"));
+// console.log(toArray<boolean>(true, false));
+// console.log(toArray<Obj>({ x: 1 }, { x: 2 }));
+// console.log(toArray<number[]>([1, 2], [3, 4, 5])); // number[]
+// console.log(toArray<Arr>([1, 2], [3, 4])); // Arr(Tuple)
+
+// Class
+class User<P> {
+  constructor(public payload: P) {}
+
+  getPayload() {
+    return this.payload;
+  }
 }
 
-console.log(toArray<number>(1, 2));
-console.log(toArray<string>("lee", "seonghyun"));
-console.log(toArray<boolean>(true, false));
-console.log(toArray<Obj>({ x: 1 }, { x: 2 }));
-console.log(toArray<number[]>([1, 2], [3, 4, 5])); // number[]
-console.log(toArray<Arr>([1, 2], [3, 4])); // Arr(Tuple)
+interface UserAType {
+  name: string;
+  age: number;
+  isValid: boolean;
+}
+
+interface UserBType {
+  name: string;
+  age: number;
+  emails: string[];
+}
+
+const coolmarvel = new User<UserAType>({
+  name: "coolmarvel",
+  age: 27,
+  isValid: true,
+  //   emails: [], // ?!
+});
+
+const windfall = new User<UserBType>({ name: "mykim", age: 4, emails: ["mykim82@wemade.com"] });
