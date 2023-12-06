12ed0b2035d595f5a85cb5706aa89f738f94392d
coolmarvel <marvel97@naver.com>
2023-12-06 17:21:15 +0900

    board service findAll & find by id(string) inject to controller

--- crud-board/src/board/board.controller.ts
+++ crud-board/src/board/board.controller.ts
@@ -7,17 +7,20 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { BoardService } from './board.service';
 
 @Controller('board')
 export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
   @Get()
   findAll() {
-    return 'findAll';
+    return this.boardService.findAll();
   }
 
   @Get(':id')
-  find(@Param('id') id: number) {
-    return `find id: ${id}`;
+  find(@Param('id') id: string) {
+    return this.boardService.find(id);
   }
 
   @Post()
--- crud-board/src/board/board.service.ts
+++ crud-board/src/board/board.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+  private boards = [
+    { id: '1', name: 'name 1', content: 'content 1' },
+    { id: '2', name: 'name 2', content: 'content 2' },
+    { id: '3', name: 'name 3', content: 'content 3' },
+    { id: '4', name: 'name 4', content: 'content 4' },
+    { id: '5', name: 'name 5', content: 'content 5' },
+  ];
+
+  findAll() {
+    return this.boards;
+  }
+
+  find(id: string) {
+    const index = this.boards.findIndex((board) => board.id === id);
+    return this.boards[index];
+  }
+}
