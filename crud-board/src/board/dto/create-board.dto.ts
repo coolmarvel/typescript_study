6c5cbdb406456e12e2a0bc0d55a58fc8b07a814c
coolmarvel <marvel97@naver.com>
2023-12-07 14:53:47 +0900

    swagger api property add

--- crud-board/src/board/dto/create-board.dto.ts
+++ crud-board/src/board/dto/create-board.dto.ts
@@ -1,11 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateBoardDto {
   @IsNotEmpty()
   @MinLength(2)
   @MaxLength(20)
+  @ApiProperty({
+    description: '이름',
+    required: true,
+    example: 'name',
+  })
   name: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    description: '내용',
+    required: true,
+    example: 'content',
+  })
   content: string;
 }
