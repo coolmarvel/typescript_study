8ff6e849fe77ba7b17df46f9f14941c62d1b492b
coolmarvel <marvel97@naver.com>
2023-12-07 14:51:46 +0900

    @nestjs/swagger at partialtype & picktype & omittype commit

--- crud-board/src/board/dto/update-board.dto.ts
+++ crud-board/src/board/dto/update-board.dto.ts
@@ -1,4 +1,6 @@
+import { OmitType, PartialType, PickType } from '@nestjs/swagger';
 import { IsOptional, MaxLength, MinLength } from 'class-validator';
+import { CreateBoardDto } from './create-board.dto';
 
 export class UpdateBoardDto {
   @IsOptional()
@@ -9,3 +11,12 @@ export class UpdateBoardDto {
   @IsOptional()
   content?: string;
 }
+
+// 주의해야 할 점 CreateBoardDto에서 @IsNotEmpty() 데코레이터 속성도 가져오기 때문에 Optional하게 사용할 수 없음.
+// export class UpdateBoardDto extends PartialType(CreateBoardDto) {}
+
+// Optional하게 사용하고 싶은 것을 pick
+// export class UpdateBoardDto extends PickType(CreateBoardDto, ['name']) {}
+
+// Optional하게 사용하고 싶지 않은 것을 omit
+// export class UpdateBoardDto extends OmitType(CreateBoardDto, ['name']) {}
