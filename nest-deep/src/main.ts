693591204adc283fd6ec8bf964d15bf3975ff27c
coolmarvel <marvel97@naver.com>
2023-12-13 14:02:09 +0900

    necessary library install & main.ts setup

--- nest-deep/src/main.ts
+++ nest-deep/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+
+  // Swagger
+  const config = new DocumentBuilder().setTitle('NestJS Project').setDescription('NestJS Project API Description').setVersion('1.0.0').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  // ValidationPipe Global Apply
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); // class-transformer apply
+
+  await app.listen(port);
+  console.log(`listening on http://localhost:${port}`);
 }
 bootstrap();
