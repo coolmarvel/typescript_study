d51dac4cc775b6ffc1f1e4f83340c18bcdfd0c76
coolmarvel <marvel97@naver.com>
2023-12-13 15:09:24 +0900

    user findall findone swagger config

--- nest-deep/src/user/user.controller.ts
+++ nest-deep/src/user/user.controller.ts
@@ -1,17 +1,27 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { UserService } from './user.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { PageReqDto } from 'src/common/dto/req.dto';
+import { User, UserAfterAuth } from 'src/common/decorator/user.decorator';
+import { ApiGetItemsResponse } from 'src/common/decorator/swagger.decorator';
+import { FindUserResDto } from './dto/res.dto';
 
 @ApiTags('User')
 @Controller('api/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiBearerAuth()
+  @ApiGetItemsResponse(FindUserResDto)
   @Get()
-  findAll() {
+  findAll(@Query() { page, size }: PageReqDto, @User() user: UserAfterAuth) {
+    console.log(user);
+
     return this.userService.findAll();
   }
 
+  @ApiBearerAuth()
+  @ApiGetItemsResponse(FindUserResDto)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
