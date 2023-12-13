a4c17502f182e2b6eb4328a53b805dca7fba46c8
coolmarvel <marvel97@naver.com>
2023-12-13 14:05:14 +0900

    typeorm config with postgres & video, auth, user, analytics module setup

--- nest-deep/src/app.module.ts
+++ nest-deep/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VideoModule } from './video/video.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { AnalyticsModule } from './analytics/analytics.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5434,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+    VideoModule,
+    AuthModule,
+    UserModule,
+    AnalyticsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
