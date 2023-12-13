ed2d15f88063eaa849bc66b531c3de83fca544da
coolmarvel <marvel97@naver.com>
2023-12-13 15:33:29 +0900

    auth module at jwt strategy & guard logic add

--- nest-deep/src/auth/auth.module.ts
+++ nest-deep/src/auth/auth.module.ts
@@ -3,12 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Module({
   imports: [UserModule, PassportModule, JwtModule.register({ global: true, secret: 'temp secret', signOptions: { expiresIn: '1d' } })],
-  providers: [AuthService, JwtService],
+  providers: [AuthService, JwtStrategy, { provide: APP_GUARD, useClass: JwtAuthGuard }],
   controllers: [AuthController],
   exports: [AuthService],
 })
