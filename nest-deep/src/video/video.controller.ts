76b0c7e22fa3a46ec54f2a6269492bfbb738a255
coolmarvel <marvel97@naver.com>
2023-12-13 15:16:45 +0900

    video controller swagger decorator config

--- nest-deep/src/video/video.controller.ts
+++ nest-deep/src/video/video.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { ApiBearerAuth, ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import { CreateVideoReqDto, FindVideoReqDto } from './dto/req.dto';
+import { CreateVideoResDto, FindVideoResDto } from './dto/res.dto';
+import { PageReqDto } from 'src/common/dto/req.dto';
+import { PageResDto } from 'src/common/dto/res.dto';
+import { ApiGetItemsResponse, ApiGetResponse, ApiPostResponse } from 'src/common/decorator/swagger.decorator';
+import { VideoService } from './video.service';
 
+@ApiTags('Video')
+@ApiExtraModels(FindVideoReqDto, FindVideoResDto, PageReqDto, CreateVideoResDto, PageResDto)
 @Controller('video')
-export class VideoController {}
+export class VideoController {
+  constructor(private readonly videoService: VideoService) {}
+
+  @ApiBearerAuth()
+  @ApiPostResponse(CreateVideoResDto)
+  @Post()
+  upload(@Body() createVideoReqDto: CreateVideoReqDto) {
+    return this.videoService.create();
+  }
+
+  @ApiBearerAuth()
+  @ApiGetItemsResponse(FindVideoResDto)
+  @Get()
+  findAll(@Query() { page, size }: PageReqDto) {
+    return this.videoService.findAll();
+  }
+
+  @ApiBearerAuth()
+  @ApiGetResponse(FindVideoResDto)
+  @Get(':id')
+  findOne(@Param() { id }: FindVideoReqDto) {
+    return this.videoService.findOne(id);
+  }
+
+  @ApiBearerAuth()
+  @Get(':id/download')
+  async download(@Param() { id }: FindVideoReqDto) {
+    return this.videoService.download(id);
+  }
+}
