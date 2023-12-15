d9224f35a8e367fd2970b8a91dd5a2a97058a90b
coolmarvel <marvel97@naver.com>
2023-12-15 12:55:50 +0900

    orm-config to migration:create setting edit

---
+++ nest-deep/src/1702612510963-migrations.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class Migrations1702612510963 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
