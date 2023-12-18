e47f7927f18b655f9bd9eb2bcc4916e4d1b7e304
coolmarvel <marvel97@naver.com>
2023-12-18 10:14:54 +0900

    find-videos handler with queryHandler

--- nest-deep/src/video/video-created.handler.ts
+++ nest-deep/src/video/video-created.handler.ts
@@ -1,6 +1,7 @@
-import { IEventHandler } from '@nestjs/cqrs';
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { VideoCreatedEvent } from './event/video.created.event';
 
+@EventsHandler(VideoCreatedEvent)
 export class VideoCreatedHandler implements IEventHandler<VideoCreatedEvent> {
   handle(event: VideoCreatedEvent) {
     console.info(`Video created(id: ${event.id})`);
