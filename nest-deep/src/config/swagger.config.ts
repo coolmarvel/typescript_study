7d680b9a3b45f5ac4ff03f95f8187b9c885f2f8c
coolmarvel <marvel97@naver.com>
2023-12-21 14:58:35 +0900

    git actions CI/CD settings update

--- nest-deep/src/config/swagger.config.ts
+++ nest-deep/src/config/swagger.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('swagger', async () => ({
   user: process.env.SWAGGER_USERNAME || 'marvel97',
-  password: process.env.SWAGGER_PASSWORD || 'password',
+  password: process.env.SWAGGER_PASSWORD || '1234',
 }));
